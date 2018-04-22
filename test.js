b164d367a37dd3239dcaca66cb6c2b68dcf39e88
Andreas Gullberg Larsen <andreas.larsen84@gmail.com>
2018-04-22 11:42:54 +0200

    Fix tests

--- test.js
+++ test.js
@@ -3,9 +3,20 @@
 var assert = require('assert');
 var yawg = require('./lib');
 
-var password = yawg({
-    minLength: 8,
-    maxLength: 15,
-});
-assert.equal(password && password.length >= 8, true, 'Password minimum length not reached.');
-assert.equal(password.length <= 15, true, 'Password maximum length exceeded.');
+for (let i=0; i<1000; i++) {
+    const phrase = yawg({
+        minLength: 15,
+        maxLength: 40,
+        minWords: 3,
+        maxWords: 5
+    });
+
+    // TODO Should add a lot more test cases here and tighter constraints, but.. meh!
+    assert.equal(phrase.length >= 15, true, 'Password minimum length not reached.');
+    assert.equal(phrase.length <= 40, true, 'Password maximum length exceeded.');
+
+    const words = phrase.split(' ');
+    assert.equal(words.length >= 3, true, 'Fewer words than minWords.');
+    assert.equal(words.length <= 5, true, 'More words than maxWords.');
+}
+
